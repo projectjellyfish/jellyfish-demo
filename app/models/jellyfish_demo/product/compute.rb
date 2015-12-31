065a0da1886d23e6e1f0ca8027852a3b6426fbf9
Colin Wiley <colinwwiley@gmail.com>
2015-12-31 08:40:05 -0600

    Add software product type

---
+++ app/models/jellyfish_demo/product/compute.rb
@@ -0,0 +1,16 @@
+module JellyfishDemo
+  module Product
+    class Compute < ::Product
+      def order_questions
+        [
+          { name: :master_username, value_type: :string, label: 'Admin Username', required: true },
+          { name: :master_password, value_type: :string, label: 'Admin Password', required: true }
+        ]
+      end
+
+      def service_class
+        'JellyfishDemo::Service::Compute'.constantize
+      end
+    end
+  end
+end
--- app/models/jellyfish_demo/product_type/compute.rb
+++ app/models/jellyfish_demo/product_type/compute.rb
@@ -1,29 +1,29 @@
 
 module JellyfishDemo
   module ProductType
-    class Software < ::ProductType
+    class Server < ::ProductType
       def self.load_product_types
         return unless super
 
         transaction do
-          [
-            set('Demo Software', '6e1c01ab-64e9-43b0-891f-5657170c845d', description: 'Demo Software Product Type', provider_type: 'JellyfishDemo::Provider::Demo')
-          ].each { |s| create! s.merge!(type: 'JellyfishDemo::ProductType::Software') }
+"#{          [
+            set('Demo Compute', '6e1c01ab-64e9-43b0-891f-5657170c845d', description: 'Demo Compute Product Type', provider_type: 'JellyfishDemo::Provider::Demo')
+          ].each { |s| create! s.merge!(type: 'JellyfishDemo::ProductType::Compute') }}"
         end
       end
 
       def description
-        'Demo Software'
+        'Demo Compute'
       end
 
       def tags
-        ['software']
+        ['demo', 'compute']
       end
 
       def product_questions
         [
           { name: :region, value_type: :string, field: :demo_regions, required: true },
-          { name: :flavor_id, value_type: :string, field: :demo_software_flavors, required: true },
+          { name: :flavor_id, value_type: :string, field: :demo_compute_flavors, required: true },
           { name: :image_id, value_type: :string, label: 'Image ID', required: true },
           { name: :subnet_id, value_type: :string, field: :demo_subnets, required: true },
           { name: :key_name, value_type: :string, label: 'Key Value', required: true }
@@ -31,8 +31,9 @@ module JellyfishDemo
       end
 
       def product_class
-        'JellyfishDemo::Product::Software'.constantize
+        'JellyfishDemo::Product::Compute'.constantize
       end
     end
   end
 end
+
