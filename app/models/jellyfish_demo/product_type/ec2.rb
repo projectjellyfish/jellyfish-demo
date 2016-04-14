7adee3c22e2f1bfa33415c820456052bfd510955
Michael Fernando <michael@Michaels-MacBook-Pro-2.local>
2016-04-14 13:19:27 -0500

    Plug in AWS products for mocking demo products

---
+++ app/models/jellyfish_demo/product_type/ec2.rb
@@ -0,0 +1,45 @@
+module JellyfishDemo
+  module ProductType
+    class Ec2 < ::ProductType
+      def self.load_product_types
+        return unless super
+
+        transaction do
+          [
+            set('EC2 Instance', 'd16d5017-6d6b-4775-958f-535f68b4cf04', provider_type: 'JellyfishDemo::Provider::Demo')
+          ].each do |s|
+            create! s.merge!(type: 'JellyfishDemo::ProductType::Ec2')
+          end
+        end
+      end
+
+      def description
+        'Amazon Web Services Elastic Compute Cloud Instance'
+      end
+
+      def tags
+        ['ec2', 'server']
+      end
+
+      def product_questions
+        [
+          { name: :flavor_id, value_type: :string, field: :aws_ec2_flavors, required: true },
+          { name: :image_id, value_type: :string, label: 'AMI ID', required: true },
+          { name: :vpc_id, value_type: :string, field: :aws_vpcs, required: true },
+          { name: :subnet_id, value_type: :string, field: :aws_subnets, required: true },
+          { name: :security_group_id, value_type: :string, field: :aws_security_groups, required: true },
+          { name: :key_name, value_type: :string, field: :aws_key_names, required: true }
+        ]
+      end
+
+      def order_questions
+        [
+        ]
+      end
+
+      def service_class
+        'JellyfishDemo::Service::Ec2'.constantize
+      end
+    end
+  end
+end
---
+++ app/models/jellyfish_demo/product_type/s3.rb
@@ -0,0 +1,39 @@
+module JellyfishDemo
+  module ProductType
+    class S3 < ::ProductType
+      def self.load_product_types
+        return unless super
+
+        transaction do
+          [
+            set('S3 Instance', '11ca0e0e-617d-45f3-b10a-acf42d5e6ecc', provider_type: 'JellyfishDemo::Provider::Demo', active: 'false')
+          ].each do |s|
+            create! s.merge!(type: 'JellyfishDemo::ProductType::S3')
+          end
+        end
+      end
+
+      def description
+        'Amazon Web Services Scalable Cloud Storage Instance'
+      end
+
+      def tags
+        ['s3', 'storage', 'cdn']
+      end
+
+      def product_questions
+        [
+        ]
+      end
+
+      def order_questions
+        [
+        ]
+      end
+
+      def service_class
+        'JellyfishDemo::Service::S3'.constantize
+      end
+    end
+  end
+end
